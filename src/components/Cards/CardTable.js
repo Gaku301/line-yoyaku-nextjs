bb9da464646ca233038e1680f907e3b6ed0afc88
Gaku Ishikawa <gaku.jp.301@gmail.com>
2022-08-07 17:44:05 +0900

    create dash board close #1

--- src/components/Cards/CardTable.js
+++ src/components/Cards/CardTable.js
@@ -23,7 +23,7 @@ export default function CardTable({ color }) {
                   (color === "light" ? "text-blueGray-700" : "text-white")
                 }
               >
-                Card Tables
+                LINE友だち一覧
               </h3>
             </div>
           </div>
--- src/pages/admin/tables.js
+++ src/pages/admin/tables.js
@@ -1,22 +1,23 @@
 import React from "react";
 
 // components
-
 import CardTable from "components/Cards/CardTable.js";
+import CardNews from "components/Cards/CardNews";
 
 // layout for page
-
 import Admin from "layouts/Admin.js";
 
 export default function Tables() {
   return (
     <>
+      <div className="flex flex-wrap">
+        <div className="w-full px-4">
+          <CardNews />
+        </div>
+      </div>
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
           <CardTable />
-        </div>
-        <div className="w-full mb-12 px-4">
-          <CardTable color="dark" />
         </div>
       </div>
     </>
